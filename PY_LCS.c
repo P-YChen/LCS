33e47211984ac988be7a7cf960a3105c95adc138
PY-Chen <py.chen1992@gmail.com>
2017-08-31 00:19:05 +0800

    com master

--- PY_LCS.c
+++ PY_LCS.c
@@ -4,7 +4,7 @@
 #include "PY_LCS.h"
 #include "PY_TEST_UTIL.h"
 
-int longest_list_substring(char *pStr1, char *pStr2)
+int longest_undo_substring(char *pStr1, char *pStr2)
 {
 	int strLen1 = 0, strLen2 = 0;
 	int **pTable = NULL;
